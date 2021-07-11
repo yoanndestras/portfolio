b4b793b5a1f7c933823203d66ef608e1315e3467
yoanndestras <73700987+yoanndestras@users.noreply.github.com>
2021-07-11 12:10:01 +0200

    mes real hover and click to github

--- js/script.js
+++ js/script.js
@@ -25,6 +25,68 @@ function typeEffect()
     speed);
 }
 
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+/*
+    let projets = document.querySelector("#projet_1");
+    let click = document.querySelector("#projet_text_1");
 
+    
+
+    projets.addEventListener("mouseenter", hover);
+    projets.addEventListener("mouseleave", leave);
+*/
+
+
+  let projets = document.querySelectorAll(".projets");
+  let click = document.querySelectorAll(".projet_text");
+
+  
+  
+
+  
+  for(let i=0 ; i<click.length; i++)
+  {
+    click[i].classList.add("opacity_0");
+  }
+
+  for(let i=0 ; i<click.length; i++)
+  {
+    for(let i=0; i<projets.length; i++){
+      projets[i].addEventListener("mouseenter", function(){
+        click[i].classList.add("opacity_1");
+      });
+     }
+     for(let i=0; i<projets.length; i++){
+      projets[i].addEventListener("mouseleave", function(){
+        click[i].classList.remove("opacity_1");
+      });
+     }
+  }
+
+  
+
+  });
 
 
